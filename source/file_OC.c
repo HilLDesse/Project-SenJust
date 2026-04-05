113448ccb4b99bf2382598ba16e658818054fb27
Rafreaakk <rafi.ardi2007@gmail.com>
2026-04-05 16:01:50 +0700

    Update: Menambahkan fitur Close File

--- source/file_OC.c
+++ source/file_OC.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <conio.h>
+
 #include "../header/File_OC.h"
+#include "../header/file_s.h"
+#include "../header/file_ec.h"
+#include "../header/screen.h"
 
 void openFile(Buffer *ed) {
     printf("Masukkan nama file yang ingin dibuka: ");
@@ -28,8 +33,31 @@ void openFile(Buffer *ed) {
             }
             printf("---------------\n");
 
+            printf("Apakah anda ingin mengedit file ini? (y/n)");
+            char edit = getch();
+            if (edit == 'y' || edit == 'Y') {
+                editFile(ed);
+            }
+
     } else {
         printf("[ERROR] File tidak ditemukan!\n");
         ed->namaFile[0] = '\0';  // untuk stop membaca file 
+    }
+}
+
+void closeFile(Buffer *ed) {
+    if (ed->input == 27 && ed->isSaved == 0) { // Kondisi user input 27 (ESC) dan file belum disave
+        system("cls");
+        printf("[PERINGATAN] File belum disave, Apakah anda ingin menyimpan? (y/n): ");
+        char simpan = getch();
+        if (simpan == 'y' || simpan == 'Y') {
+            saveFile(ed);
+            printf("\nLanjut edit file atau kembali menu utama? (y/n): ");
+            char lanjut = getch();
+            if (lanjut == 'y' || lanjut == 'Y') {
+                printLayar(ed, ed->b_now, ed->k_now);
+                editFile(ed);
+            }
+        }
     }
 }
