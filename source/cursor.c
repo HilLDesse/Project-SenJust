8b8f0ad8b9ed709405309d2b9ea8775f42b55496
AHMAD AISY FATHRYAN SISWANDAR_1B_D3 <ahmadaisy12@gmail.com>
2026-04-05 17:54:50 +0700

    Update: Enhance text selection and clipboard functionality

--- source/cursor.c
+++ source/cursor.c
@@ -3,14 +3,17 @@
 #include <conio.h>
 #include <string.h>
 #include <ctype.h>
-#include <windows.h>
+#include <windows.h> 
 
 #include "../header/cursor.h"
 #include "../header/screen.h"
 #include "../header/buffer.h"
 #include "../header/file_s.h"
 
-// Fungsi memindahkan kursor visual
+#define KEY_DELETE 83 
+
+static char clipboard[10000] = "";
+
 void gotoXY(int x, int y) {
     COORD coord;
     coord.X = x; 
@@ -18,16 +21,21 @@ void gotoXY(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-void cetakHighlight(Buffer *buff, int start_b, int start_k, int end_b, int end_k) {
-    if (start_b == -1) return; // Jika tidak ada yang diseleksi, batalkan
-
-    int b1 = start_b, k1 = start_k;
-    int b2 = end_b, k2 = end_k;
-
-    if (b1 > b2 || (b1 == b2 && k1 > k2)) {
-        b1 = end_b; k1 = end_k;
-        b2 = start_b; k2 = start_k;
+void getSelectionBounds(int *b1, int *k1, int *b2, int *k2, int b_now, int k_now, int sel_b, int sel_k) {
+    if (sel_b < b_now || (sel_b == b_now && sel_k < k_now)) {
+        *b1 = sel_b; *k1 = sel_k;
+        *b2 = b_now; *k2 = k_now;
+    } else {
+        *b1 = b_now; *k1 = k_now;
+        *b2 = sel_b; *k2 = sel_k;
     }
+}
+
+void cetakHighlight(Buffer *buff, int start_b, int start_k, int end_b, int end_k) {
+    if (start_b == -1) return;
+
+    int b1, k1, b2, k2;
+    getSelectionBounds(&b1, &k1, &b2, &k2, end_b, end_k, start_b, start_k);
 
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -40,18 +48,62 @@ void cetakHighlight(Buffer *buff, int start_b, int start_k, int end_b, int end_k
             gotoXY(c, r);
             // Ubah warna menjadi Background Putih, Text Hitam (Efek Block)
             SetConsoleTextAttribute(hConsole, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE);
-            printf("%c", buff->teks[r][c]); // Cetak ulang hurufnya di atas cetakan temanmu
+            printf("%c", buff->teks[r][c]); 
         }
     }
-
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 }
 
-// 1. FUNGSI PERGERAKAN KURSOR DASAR
+void copyText(Buffer *buff, int sel_b, int sel_k) {
+    int b1, k1, b2, k2;
+    getSelectionBounds(&b1, &k1, &b2, &k2, buff->b_now, buff->k_now, sel_b, sel_k);
+    
+    int clipIndex = 0;
+    clipboard[0] = '\0'; 
+    
+    for (int r = b1; r <= b2; r++) {
+        int len = strlen(buff->teks[r]);
+        int c_start = (r == b1) ? k1 : 0;
+        int c_end = (r == b2) ? k2 : len;
+        
+        for (int c = c_start; c < c_end; c++) {
+            clipboard[clipIndex++] = buff->teks[r][c];
+        }
+        if (r < b2) clipboard[clipIndex++] = '\n';
+    }
+    clipboard[clipIndex] = '\0'; 
+}
+
+void deleteSelection(Buffer *buff, int sel_b, int sel_k) {
+    int b1, k1, b2, k2;
+    getSelectionBounds(&b1, &k1, &b2, &k2, buff->b_now, buff->k_now, sel_b, sel_k);
+    
+    buff->b_now = b2;
+    buff->k_now = k2;
+    
+    while (buff->b_now > b1 || (buff->b_now == b1 && buff->k_now > k1)) {
+        deleteHuruf(buff, &buff->b_now, &buff->k_now);
+    }
+}
+
+void cutText(Buffer *buff, int sel_b, int sel_k) {
+    copyText(buff, sel_b, sel_k); 
+    deleteSelection(buff, sel_b, sel_k); 
+}
+
+void pasteText(Buffer *buff) {
+    for (int i = 0; clipboard[i] != '\0'; i++) {
+        if (clipboard[i] == '\n') {
+            newBaris(buff, &buff->b_now, &buff->k_now);
+        } else {
+            insertHuruf(buff, &buff->b_now, &buff->k_now, clipboard[i]);
+        }
+    }
+}
+
 void moveLeft(Buffer *buff) {
-    if (buff->k_now > 0) {
-        buff->k_now--; 
-    } else if (buff->b_now > 0) { 
+    if (buff->k_now > 0) buff->k_now--; 
+    else if (buff->b_now > 0) { 
         buff->b_now--;
         buff->k_now = strlen(buff->teks[buff->b_now]);
     }
@@ -59,9 +111,8 @@ void moveLeft(Buffer *buff) {
 
 void moveRight(Buffer *buff) {
     int panjang_baris = strlen(buff->teks[buff->b_now]);
-    if (buff->k_now < panjang_baris) {
-        buff->k_now++; 
-    } else if (buff->b_now < buff->total_baris - 1) { 
+    if (buff->k_now < panjang_baris) buff->k_now++; 
+    else if (buff->b_now < buff->total_baris - 1) { 
         buff->b_now++;
         buff->k_now = 0;
     }
@@ -71,9 +122,7 @@ void moveUp(Buffer *buff) {
     if (buff->b_now > 0) {
         buff->b_now--; 
         int panjang_atas = strlen(buff->teks[buff->b_now]);
-        if (buff->k_now > panjang_atas) {
-            buff->k_now = panjang_atas; 
-        }
+        if (buff->k_now > panjang_atas) buff->k_now = panjang_atas; 
     }
 }
 
@@ -81,9 +130,7 @@ void moveDown(Buffer *buff) {
     if (buff->b_now < buff->total_baris - 1) {
         buff->b_now++; 
         int panjang_bawah = strlen(buff->teks[buff->b_now]);
-        if (buff->k_now > panjang_bawah) {
-            buff->k_now = panjang_bawah;
-        }
+        if (buff->k_now > panjang_bawah) buff->k_now = panjang_bawah;
     }
 }
 
@@ -134,44 +181,86 @@ void editorKursor(Buffer *buff)
         if (buff->input == -32 || buff->input == 224 || buff->input == 0) 
         {
             int arrow = getch(); 
-
-            // Cek apakah tombol Shift di-keyboard sedang ditekan (0x8000 adalah kodenya)
             int isShiftPressed = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
 
-            // Logika Blok Teks
             if (isShiftPressed && !is_selecting) {
-                // Baru mulai nge-blok
                 is_selecting = 1;
                 sel_start_b = buff->b_now;
                 sel_start_k = buff->k_now;
-            } else if (!isShiftPressed && is_selecting) {
-                // Shift dilepas, batalkan blok
-                is_selecting = 0; 
-                sel_start_b = -1;
-                sel_start_k = -1;
             }
 
-            switch (arrow) {
-                case KEY_UP: moveUp(buff); break;
-                case KEY_DOWN: moveDown(buff); break;
-                case KEY_LEFT: moveLeft(buff); break;
-                case KEY_RIGHT: moveRight(buff); break;
-                case CTRL_LEFT: moveWordLeft(buff); break;
-                case CTRL_RIGHT: moveWordRight(buff); break;
+            if (arrow == KEY_DELETE) 
+            {
+                if (is_selecting) {
+                    deleteSelection(buff, sel_start_b, sel_start_k);
+                    is_selecting = 0; sel_start_b = -1; sel_start_k = -1;
+                } else {
+                    int old_b = buff->b_now, old_k = buff->k_now;
+                    moveRight(buff);
+                    if (buff->b_now != old_b || buff->k_now != old_k) {
+                        deleteHuruf(buff, &buff->b_now, &buff->k_now);
+                    }
+                }
+                buff->isSaved = 0;
+            } 
+            else 
+            {
+                if (!isShiftPressed && is_selecting) {
+                    is_selecting = 0; 
+                    sel_start_b = -1; sel_start_k = -1;
+                }
+
+                switch (arrow) {
+                    case KEY_UP: moveUp(buff); break;
+                    case KEY_DOWN: moveDown(buff); break;
+                    case KEY_LEFT: moveLeft(buff); break;
+                    case KEY_RIGHT: moveRight(buff); break;
+                    case CTRL_LEFT: moveWordLeft(buff); break;
+                    case CTRL_RIGHT: moveWordRight(buff); break;
+                }
             }
 
             printLayar(buff, buff->b_now, buff->k_now);
-            
-            if (is_selecting) {
-                cetakHighlight(buff, sel_start_b, sel_start_k, buff->b_now, buff->k_now);
-            }
-
+            if (is_selecting) cetakHighlight(buff, sel_start_b, sel_start_k, buff->b_now, buff->k_now);
             gotoXY(buff->k_now, buff->b_now); 
+        }
+        else if (buff->input == 3)
+        {
+            if (is_selecting) {
+                copyText(buff, sel_start_b, sel_start_k);
+                is_selecting = 0; sel_start_b = -1; sel_start_k = -1;
+                printLayar(buff, buff->b_now, buff->k_now);
+                gotoXY(buff->k_now, buff->b_now);
+            }
+        }
+        else if (buff->input == 24)
+        {
+            if (is_selecting) {
+                cutText(buff, sel_start_b, sel_start_k);
+                is_selecting = 0; sel_start_b = -1; sel_start_k = -1;
+                buff->isSaved = 0;
+                printLayar(buff, buff->b_now, buff->k_now);
+                gotoXY(buff->k_now, buff->b_now);
+            }
+        }
+        else if (buff->input == 22)
+        {
+            if (is_selecting) {
+                deleteSelection(buff, sel_start_b, sel_start_k);
+                is_selecting = 0; sel_start_b = -1; sel_start_k = -1;
+            }
+            pasteText(buff);
+            buff->isSaved = 0;
+            printLayar(buff, buff->b_now, buff->k_now);
+            gotoXY(buff->k_now, buff->b_now);
         }
         // --- DETEKSI KARAKTER BIASA ---
         else if (buff->input >= 32 && buff->input <= 126) 
         { 
-            is_selecting = 0; // Ketik huruf biasa = batal blok
+            if (is_selecting) {
+                deleteSelection(buff, sel_start_b, sel_start_k);
+                is_selecting = 0; sel_start_b = -1; sel_start_k = -1;
+            }
             insertHuruf(buff, &buff->b_now, &buff->k_now, buff->input);
             printLayar(buff, buff->b_now, buff->k_now);
             buff->isSaved = 0;
@@ -179,15 +268,22 @@ void editorKursor(Buffer *buff)
         }
         else if (buff->input == 8)  // Backspace
         {
-            is_selecting = 0; // Hapus huruf = batal blok
-            deleteHuruf(buff, &buff->b_now, &buff->k_now);
+            if (is_selecting) {
+                deleteSelection(buff, sel_start_b, sel_start_k);
+                is_selecting = 0; sel_start_b = -1; sel_start_k = -1;
+            } else {
+                deleteHuruf(buff, &buff->b_now, &buff->k_now);
+            }
             printLayar(buff, buff->b_now, buff->k_now);
             buff->isSaved = 0;
             gotoXY(buff->k_now, buff->b_now); 
         }
         else if (buff->input == 13) // Enter
         {
-            is_selecting = 0; // Baris baru = batal blok
+            if (is_selecting) {
+                deleteSelection(buff, sel_start_b, sel_start_k);
+                is_selecting = 0; sel_start_b = -1; sel_start_k = -1;
+            }
             newBaris(buff, &buff->b_now, &buff->k_now);
             printLayar(buff, buff->b_now, buff->k_now);
             buff->isSaved = 0;
