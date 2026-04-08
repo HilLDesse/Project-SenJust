deb945f6b02b578774a5503e7e0a741a621cae40
HilLDesse <hilmikaut@gmail.com>
2026-04-09 00:10:33 +0700

    Update: Menambahkan header yang tidak ter push

--- header/buffer.h
+++ header/buffer.h
@@ -16,6 +16,7 @@ typedef struct
     char namanewFile[100];
     char namaoldFile[100];
     int isSaved;
+    int autoSaveOn;
     char input;
     int next;
     int b_now;
--- header/file_s.h
+++ header/file_s.h
@@ -6,5 +6,6 @@
 void saveFile(Buffer *buff); // Fungsi untuk menyimpan file yang diedit
 void shortcutSave(Buffer *buff); // Fungsi untuk menyimpan file dengan shortcut (CTRL + S)
 void saveAS(Buffer *buff); // Fungsi untuk menyimpan file dengan nama baru (CTRL + SHIFT + S)
+void autoSave(Buffer *buff); // Fungsi untuk autosave dengan shortcut (CTRL + T)
 
 #endif
