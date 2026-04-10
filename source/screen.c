d9b583f737279cddf1c979f6bead1d957ec33101
AHMAD AISY FATHRYAN SISWANDAR_1B_D3 <ahmadaisy12@gmail.com>
2026-04-10 21:22:04 +0700

    Update: Refined edit file screen

--- source/screen.c
+++ source/screen.c
@@ -11,6 +11,8 @@ void printLayar(Buffer *b, int baris_sekarang, int kolom_sekarang)
         printf("%s\n", b->teks[i]); // Menampilkan isi tiap baris ke layar
     }
     
-    printf("\n=======================================\n");
+    printf("\n------------------------------------------------------------------------------------------------------\n");
+    printf("|CTRL+S:Save | CTRL+Q:Copy | CTRL+B:Paste | CTRL+Z:Undo | CTRL+Y:Redo | CTRL+F:Find | CTRL+T:Autosave|");
+    printf("\n------------------------------------------------------------------------------------------------------\n");
     printf("Tekan ESC untuk keluar aplikasi.\n");
 }
